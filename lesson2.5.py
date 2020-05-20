6f6d6c2bea6d78133897f9314790a9316bdabb28
Aleksandra Doronina <Shurara-san@yandex.ru>
2020-05-20 15:30:45 +0300

    lesson2.5

--- lesson2.5.py
+++ lesson2.5.py
@@ -8,15 +8,18 @@
 """
 
 my_list = [78, 56, 56, 34, 18, 10, 5, 4, 3, 2, 1]
-
+n = 0
 number = int(input("Введите число\n"))
 if number in my_list:
-    my_list.insert(my_list.index(number), number)
+    pos = my_list.index(number) + my_list.count(number)
+    my_list.insert(pos, number)
 else:
     for i in my_list:
         if i < number:
             my_list.insert(my_list.index(i), number)
+            n = 1
             break
-    my_list.append(number)
+    if n == 0:
+        my_list.append(number)
 
 print(my_list)
